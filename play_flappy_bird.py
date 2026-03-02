197819fa78bdf835c710211e6b35f8bf86158d38
Acee Zai Mendez <zaiakari7@gmail.com>
2026-03-02 20:04:27 +0800

    Changed folder name as the first folder is errouneous. Added new progress.

--- play_flappy_bird.py
+++ play_flappy_bird.py
@@ -12,35 +12,25 @@ pipe_gap_size = 100
 base_y = screen_height * 0.79
 images, sounds, hitmasks = {}, {}, {}
 
-PLAYERS_LIST = (
-    # red box
-    (
-        'assets/sprites/redbox-upflap.png',
-        'assets/sprites/redbox-midflap.png',
-        'assets/sprites/redbox-downflap.png',
-    ),
-    # blue box
-    (
-        'assets/sprites/bluebox-upflap.png',
-        'assets/sprites/bluebox-midflap.png',
-        'assets/sprites/bluebox-downflap.png',
-    ),
-    # yellow box
-    (
-        'assets/sprites/yellowbox-upflap.png',
-        'assets/sprites/yellowbox-midflap.png',
-        'assets/sprites/yellowbox-downflap.png',
-    ),
+players_list = (
+    ('sprites/redbird-upflap.png', 'sprites/redbird-midflap.png', 'sprites/redbird-downflap.png'),
+    ('sprites/bluebird-upflap.png', 'sprites/bluebird-midflap.png', 'sprites/bluebird-downflap.png'),
+    ('sprites/yellowbird-upflap.png', 'sprites/yellowbird-midflap.png', 'sprites/yellowbird-downflap.png'),
 )
 
-# list of backgrounds
-BACKGROUNDS_LIST = (
-    'assets/sprites/background-day.png',
-    'assets/sprites/background-night.png',
-)
+backgrounds_list = ('sprites/background-day.png', 'sprites/background-night.png')
+pipes_list = ('sprites/pipe-green.png', 'sprites/pipe-red.png')
 
-# list of pipes
-PIPES_LIST = (
-    'assets/sprites/pipe-green.png',
-    'assets/sprites/pipe-red.png',
-)
+def main():
+    global screen, fps_clock
+    pygame.init()
+    fps_clock = pygame.time.Clock()
+    
+    screen = pygame.display.set_mode((screen_width, screen_height), pygame.SCALED | pygame.FULLSCREEN)
+    pygame.display.set_caption('Flappy Box')
+
+    images['numbers'] = tuple(pygame.image.load(f'sprites/{i}.png').convert_alpha() for i in range(10))
+    images['gameover'] = pygame.image.load('sprites/gameover.png').convert_alpha()
+    images['message'] = pygame.image.load('sprites/message.png').convert_alpha()
+    images['base'] = pygame.image.load('sprites/base.png').convert_alpha()
+    
